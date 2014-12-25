cb0a3ff1c67d3904744252cabb4b771368ba27bd
Mani bhushan <manibhushan.cs@gmail.com>
2014-12-25 19:36:43 +0530

    hello world in c - after a long time :)

---
+++ 6_087_prog_c/hello.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  first program in the course of 6.087 - practical programming in C
+ *
+ *        Version:  1.0
+ *        Created:  Thursday 25 December 2014 07:11:54  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Mani Bhushan (), manibhushan.cs@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include<stdio.h>
+
+int main(void) {
+    puts("Hello 6.087 - \"Practical programming in C\" Students!");
+    return 0;
+}
+

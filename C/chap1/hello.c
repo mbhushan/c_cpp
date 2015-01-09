1daa2bd66cc84b6bfa9515b3c5ca391025ade214
Mani bhushan <manibhushan.cs@gmail.com>
2015-01-09 21:58:11 +0530

    hello world in C

---
+++ C/chap1/hello.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  first hello world program from K&R
+ *
+ *        Version:  1.0
+ *        Created:  Friday 09 January 2015 09:18:20  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Mani Bhushan (), manibhushan.cs@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+main() {
+    printf("Hello World\n");
+}
+
+

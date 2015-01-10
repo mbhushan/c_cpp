e151f2c50cfe902b84c0c2354efd58504f392b9f
Mani bhushan <manibhushan.cs@gmail.com>
2015-01-10 23:03:44 +0530

    printing EOF value in C

---
+++ C/chap1/eof_val.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  eof_val.c
+ *
+ *    Description:  finding the value of EOF
+ *
+ *        Version:  1.0
+ *        Created:  Saturday 10 January 2015 10:56:49  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Mani Bhushan (), manibhushan.cs@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+    int val = EOF;
+    printf ("EOF value is: %d\n", val);
+    return 0;
+}
+
+
+
